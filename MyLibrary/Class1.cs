5d2581d9803bca4890721454e2f11f6434fce540
Surya Prakash <suryavmds7000@gmail.com>
2024-12-12 15:07:13 -0500

    first commit

---
+++ MyLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace MyLibrary
+{
+    public class Printer
+    {
+        public void PrintMessage()
+        {
+            System.Console.WriteLine("Hello from MyLibrary!");
+        }
+    }
+}
